57224a9e98063f1685dbec7da807e76f5a1ad743
SnomeaD <snomead@gmail.com>
2012-01-13 07:21:36 +0100

    Raid change, not tested

--- SnoUI/modules/raidframes/raid.lua
+++ SnoUI/modules/raidframes/raid.lua
@@ -83,15 +83,17 @@ local function EditUnitFrame(frame, header)
 		name:SetPoint("TOP", 0, 0)
 		name:SetFont(C.media.pixelfont, 12, "MONOCHROMEOUTLINE")
 		health.colorClass = true
-		power.colorPower = true
-		name.colorPower = true
+		power.colorPower = false
+		name.colorClass = false
 		name:SetTextColor(1,1,1,1)
+		power:SetTexture(0.6,0.6,0.6)
+		power:SetVertexColor(0.6,0.6,0.6)
 		
-		-- LFDRole:Height(6*T.raidscale)
-		-- 	    LFDRole:Width(6*T.raidscale)
-		-- LFDRole:Point("TOPRIGHT", -2, -2)
+		LFDRole:Height(6*T.raidscale)
+		LFDRole:Width(6*T.raidscale)
+		LFDRole:Point("TOPRIGHT", -2, -2)
 		-- LFDRole:SetTexture("Interface\\AddOns\\Tukui\\medias\\textures\\lfdicons.blp")
-		-- self.LFDRole = LFDRole
+		frame.LFDRole = LFDRole
 		
 		raiddebuff:Height(16)
 		raiddebuff:Width(16)
